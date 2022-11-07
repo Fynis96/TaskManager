fb2a57958e65609ed3afcffec7f55f61ca1ad7c9
Fynis96 <C.hearth96@gmail.com>
2022-11-07 00:09:03 -0500

    stripped react app down, added some structure for project

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ My own run at a ToDo app using Express and React
 The current working routes in this app are:
 Post for login,
 Get/Post for users,
-Get/Post for tasks.
+All routes for tasks.
+
+
+- Create Delete/Put routes for users
--- frontend/task-manager/src/App.js
+++ frontend/task-manager/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react'
 
-function App() {
+const App = () => {
+  // const [tasks, setTasks] = useState([])
+  // const [username, setUsername] = useState('')
+  // const [password, setPassword] = useState('')
+  // const [user, setUser] = useState(null)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
